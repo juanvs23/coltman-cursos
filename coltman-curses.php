96c6369a4abad9442fed68e100e81784768a7742
Juan Carlos Avila <juanvs23@gmail.com>
2020-10-09 11:40:54 -0400

    initial comic

---
+++ coltman-curses.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Cursos Cened
+ * Plugin URI:        https://github.com/juanvs23/coltman-doctores.git
+ * Description:       Listado de personal medico
+ * Version:           1.2.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Juan Carlos Avila
+ * Author URI:        http://coltman-desing.tk/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       doctor-cened
+ * Domain Path:       /languages
+ * */
